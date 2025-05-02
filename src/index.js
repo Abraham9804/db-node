7b4be806a30b47c014fdc0f083642646d2dcfa82
Abraham Romero <romero9804@hotmail.com>
2025-05-01 18:46:43 -0700

    feat: agregar metodos put y delete

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const sequelize = require("./conexion.js")
 //Inicializar la app de express
 const app = express()
 
+//Usar json en solicitudes
+app.use(express.json())
+
 //api rest
 app.get("/", function(req, resp){
     resp.send("Hola mundo")
@@ -16,9 +19,37 @@ app.get("/categoria",async function(req, resp){
 })
 
 app.get("/productos",async function(req, resp){
-    const [results, metadata] = await sequelize.query("select * from productos")
+    const [results] = await sequelize.query("select * from productos")
     const productos = results
     resp.json(productos)
+
+})
+
+app.post("/productos", async function(req, resp){
+    console.log(req.body)
+    let nombre = req.body.nombre 
+    let precio = req.body.precio 
+    const sql = "INSERT INTO productos (nombre, precio) VALUES (?,?)"
+    await sequelize.query(sql,[nombre, precio])
+    resp.json({"mensaje":"guardado"})
+})
+
+app.put("/productos/:id", async function(req, resp){
+    let nombre = req.body.nombre 
+    let precio = req.body.precio
+    let id =     req.params.id
+
+    const sql = ("UPDATE productos SET nombre = '"+nombre+"', precio = '"+precio+"' WHERE id = '"+id+"'")
+    await sequelize.query(sql)
+    return resp.json({"message":"producto actualizado"})
+})
+
+app.delete('/productos/:id', async function(req, resp){
+    const id = req.params.id 
+
+    const sql = ("DELETE FROM productos WHERE id = '"+id+"'")
+    await sequelize.query(sql)
+    return resp.json({"message":"producto eliminado"})
 })
 
 //Levantar server
